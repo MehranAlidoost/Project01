33d7a83e99505c665cdd87e543da5cc6ba3a0f8b
MehranAlidoost <mehran.alidoost888@gmail.com>
2016-10-07 10:18:35 +0330

    first commit

---
+++ OneApp/app/src/main/java/com/testapp/oneapp/SettingActivity.java
@@ -0,0 +1,70 @@
+package com.testapp.oneapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingActivity extends AppCompatActivity {
+
+    EditText ipEditText, portEditText;
+    Button saveButton, backButton;
+    String ip, port;
+    Context context = SettingActivity.this;
+
+    SharedPreferences prefs = null;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        init();
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        ipEditText.setText(prefs.getString("ip", ""));
+        portEditText.setText(prefs.getString("port", ""));
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ipEditText.getText().toString().equals("") || portEditText.getText().toString().equals("")) {
+                    Toast.makeText(SettingActivity.this, R.string.fillAllFields, Toast.LENGTH_SHORT).show();
+                } else {
+//                    prefs = getSharedPreferences("h", MODE_PRIVATE);
+                    prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString("ip", ipEditText.getText().toString());
+                    editor.putString("port", portEditText.getText().toString());
+                    editor.apply();
+                    Toast.makeText(SettingActivity.this, R.string.ipAndPortSaved, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+
+    private void init() {
+
+        ipEditText = (EditText) findViewById(R.id.settingActivityServerIPEditText);
+        portEditText = (EditText) findViewById(R.id.settingActivityServerPortEditText);
+        saveButton = (Button) findViewById(R.id.settingActivitySaveButton);
+        backButton = (Button) findViewById(R.id.settingActivityBackButton);
+
+    }
+
+}
